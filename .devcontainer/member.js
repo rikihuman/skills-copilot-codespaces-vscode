6db673f181760fe8d26fea3ab3d74d4c3ae056b6
Riki Human <44641999+rikihuman@users.noreply.github.com>
2023-08-23 09:14:38 +0000

    Secont Copilot Commit

---
+++ .devcontainer/member.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+  return [
+    {
+      name: 'name',
+      type: 'input',
+      message: 'What is your name?',
+      default: 'John Doe',
+    },
+    {
+      name: 'email',
+      type: 'input',
+      message: 'What is your email?',
+      default: '',
+    },
+]
+}
